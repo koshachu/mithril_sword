3fc03f7f4f4a6476c3e92d17d0aad74501c91d76
koshachu <koshachu@gmail.com>
2021-06-06 19:45:04 +1000

    target setup
    - adding terrain to the main scene
    - setting target using raycast

---
+++ Mithril Sword/Assets/Scripts/InputController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public GameObject Target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Target.transform.position = hit.point;
+            }
+        }
+        
+    }
+}
